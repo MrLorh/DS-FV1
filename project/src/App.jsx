5387717e4b462a843b9adeeb4f987c4358e92af4
MrLorh <163945695+MrLorh@users.noreply.github.com>
2024-11-01 19:31:25 +0000

    asd

---
+++ project/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Page1 from "./pages/Page1.jsx"
+
+
+function App() {
+
+  return (
+    <>
+      <Page1 />
+    </>
+  )
+}
+
+export default App
---
+++ project/src/pages/Page1.jsx
@@ -0,0 +1,21 @@
+function Page1() {
+
+    return (
+        <div className="max-w-screen-md mx-auto bg-white rounded-xl shadow-md overflow-hidden p-10 grid grid-cols-3">
+            <span>
+                Selección de categoría
+            </span>
+            <button>
+                Primera División
+            </button>
+            <button>
+                Segunda División
+            </button>
+            <button>
+                Fútbol Sala
+            </button>
+        </div>
+    )
+}
+
+export default Page1
